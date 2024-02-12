07211414a7fc261e1f8fbc353b09e62688cbf55f
GSavathrakis <savathgeo@gmail.com>
2024-02-12 10:53:55 +0000

    Histogram update

--- Histograms/angles_hist.py
+++ Histograms/angles_hist.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser('Select annotations folder', add_help=False)
 parser.add_argument('--annotation_path', type=str)
 parser.add_argument('--dataset_type', type=str, choices=['DOTA_v1.5','HRSC2016','ShipRSImageNet'])
 parser.add_argument('--bin_granularity', type=int, default=10)
+parser.add_argument('--image_save_name', type=str)
 args = parser.parse_args() 
 
 def main(args):
@@ -18,12 +19,12 @@ def main(args):
 	#print(hist.sum(axis=0))
 	
 	plt.figure(figsize=(7,6))
-	plt.vlines(np.linspace(0,170,18), 0, hist.sum(axis=0), linewidth=20)
-	plt.xticks(np.linspace(0,170,18), fontsize=10)
+	plt.vlines(np.linspace(0,170,180//args.bin_granularity), 0, hist.sum(axis=0), linewidth=20)
+	plt.xticks(np.linspace(0,170,180//args.bin_granularity), fontsize=10)
 	plt.yticks()
 	plt.xlabel('Orientations (deg)', fontsize=15)
 	plt.ylabel('# Objects', fontsize=15)
-	plt.savefig('angle_dist_DOTA_ISO')
+	plt.savefig(args.image_save_name)
 	plt.close()
 	
 
