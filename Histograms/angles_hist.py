b4f8607d9933563244250dfba532edd2afd18685
GSavathrakis <savathgeo@gmail.com>
2024-02-12 10:52:13 +0000

    Histogram update

--- Histograms/angles_hist.py
+++ Histograms/angles_hist.py
@@ -6,12 +6,6 @@ import sys
 sys.path.append("/workspace/Augmentation")
 from utils.distributions import histogram_calc
 
-#cl_n_angs = np.loadtxt('/workspace/classes_n_angles_spl_1pt_06.txt', delimiter=' ')
-#class_names = #np.loadtxt('Path to csv file with class names', dtype=str)
-#classes = cl_n_angs[:,0]
-#angles = cl_n_angs[:,1]
-#angles_corr = angles[np.where(angles<180)]
-
 parser = argparse.ArgumentParser('Select annotations folder', add_help=False)
 parser.add_argument('--annotation_path', type=str)
 parser.add_argument('--dataset_type', type=str, choices=['DOTA_v1.5','HRSC2016','ShipRSImageNet'])
@@ -35,22 +29,3 @@ def main(args):
 
 if __name__ == '__main__':
 	main(args)
-
-"""
-plt.figure(figsize=(5,5))
-plt.hist(angles_corr, bins=18)
-plt.xlabel('Orientation (deg)')
-plt.ylabel('# Objects')
-plt.savefig('angle_histogram_DOTA')
-plt.close()
-
-
-for i in np.unique(classes):
-	plt.figure(figsize=(5,5))
-	plt.hist(angles[np.where(classes==i)], bins=50)
-	plt.xlabel('Orientation (deg)')
-	plt.ylabel('# Objects')
-	plt.title(f'{class_names[int(i)]} class angle histogram')
-	plt.savefig(f'angle_histograms_per_class_best/{class_names[int(i)]}_hist_1.jpg')
-	plt.close()
-"""
